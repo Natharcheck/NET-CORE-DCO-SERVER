6ca35a5ea2f78cb8a22acd7e4e9a8a9ea19c3dab
Nathar <144685491+Natharcheck@users.noreply.github.com>
2024-05-08 00:51:29 +0300

    Add files via upload

--- NET CORE DCO SERVER/Program.cs
+++ NET CORE DCO SERVER/Program.cs
@@ -19,6 +19,8 @@ namespace Server_DCO
             NetworkConfig.socket.StartListening(8000,5, 1);
             Database.CorrectPath();
 
+            Database.CreateAccount(1, "ilopuke@gmail.com", "admin", "admin");
+
             Console.WriteLine("Server started.");
         }
 
